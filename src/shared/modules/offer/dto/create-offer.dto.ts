efb58eefd9561933b862f88a3eb37924f8679e59
Reyney91 <andrew011901@mail.ru>
2024-04-13 23:34:13 +0500

    refactor: change naming in offer

--- src/shared/modules/offer/dto/create-offer.dto.ts
+++ src/shared/modules/offer/dto/create-offer.dto.ts
@@ -1,22 +1,22 @@
 import { Coordinates, FacilitiesType, PlaceType, User } from '../../../types';
 
 export class CreateOfferDto {
-  public name: string;
+  public title: string;
   public description: string;
   public postDate: Date;
   public city: string;
   public imagePreview: string;
-  public placeImages: string[];
+  public images: string[];
   public isPremium: boolean;
-  public isFavorited: boolean;
+  public isFavorite: boolean;
   public rating: number;
-  public placeType: PlaceType;
-  public countOfRooms: number;
-  public countOfGuests: number;
-  public rentPrice: number;
-  public facilities: FacilitiesType[];
+  public type: PlaceType;
+  public numberRooms: number;
+  public numberGuests: number;
+  public price: number;
+  public conveniences: FacilitiesType[];
   public author: User;
-  public countOfComments: number;
+  public commentsCount: number;
   public coordinates: Coordinates;
   public userId: string;
 }
